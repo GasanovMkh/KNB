8c6b8a4b5a223d8df37f0809dbc54232e2835bee
Arsen <arsen.ablaev0@gmail.com>
2017-03-27 22:42:52 +0300

    Question model and Questions collection have been added

---
+++ TestBL/DB/QuestionsStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TestBL.DB
+{
+    public class QuestionsStore
+    {
+        public static Dictionary<Question, List<string>> Questions = new Dictionary<Question, List<string>>
+        {
+            {new Question {Id = 1,Name = "How to?"},
+                new List<string>() {"ответ1","ответ2"} },
+
+             {new Question {Id = 2,Name = "How to2?"},
+                new List<string>() {"ответ1","ответ2"} }
+        };
+    }
+}
---
+++ TestBL/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace TestBL
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
